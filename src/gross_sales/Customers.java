509906d6b4ab5b376cdfe91e26fc4bad1aeb1d1f
Gross3LIA5 <gro19823@spengergasse.at>
2020-05-28 10:47:57 +0200

    add class Customers

--- src/gross_sales/Customers.java
+++ src/gross_sales/Customers.java
@@ -1,5 +1,20 @@
 package gross_sales;
 
-public class Customers {
+import java.util.ArrayList;
 
+public class Customers 
+{
+	private ArrayList<Customer> customers;
+	public Customers()
+	{
+		customers = new ArrayList<Customer>();
+	}
+	public ArrayList<Customer> getCustomers() 
+	{
+		return customers;
+	}
+	public void setCustomers(ArrayList<Customer> customers) 
+	{
+		this.customers = customers;
+	}
 }
